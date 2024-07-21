3aa53c22dbcdade7e4f9fd0b40e19a5449ed8e5a
vuan201 <vuan201@gmail.com>
2024-07-21 21:34:09 +0700

    refactor[style] chỉnh sửa cho các ảnh style lưu trên server

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,7 +1,6 @@
 export { default as brandsApi } from "./brandsApi";
 export { default as categoriesApi } from "./categoriesApi";
 export { default as productsApi } from "./productsApi";
-export { default as stylesApi } from "./stylesApi";
 export { default as colorsApi } from "./colorsApi";
 export { default as authApi } from "./authApi";
 export { default as sizesApi } from "./sizeApi";
